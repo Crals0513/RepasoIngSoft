62331dca0f4dacce44065c94ee7c6996ee7623ab
Cristopher Alejandro Santana Lugo <casantanal@centromotosdelhuila.com>
2026-02-05 17:56:58 -0500

    Creando Proyecto

---
+++ src/main/java/org/yourcompany/yourproject/MiPrimeraVez.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package org.yourcompany.yourproject;
+
+/**
+ *
+ * @author casantanal
+ */
+public class MiPrimeraVez {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
