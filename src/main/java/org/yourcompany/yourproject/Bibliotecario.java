89347f028d9b058c0e72a671e7023827816fc962
Cristopher Alejandro Santana Lugo <casantanal@centromotosdelhuila.com>
2026-02-10 17:18:52 -0500

    Clase Bibliotecario

---
+++ src/main/java/org/yourcompany/yourproject/Bibliotecario.java
@@ -0,0 +1,12 @@
+package org.yourcompany.yourproject;
+
+public class Bibliotecario extends Usuario{
+    private String turno;
+    public Bibliotecario(String id, String nombre, String email) {
+        super(id, nombre, email);
+    }
+
+    public String getTurno() {
+        return this.turno;
+    }
+}
