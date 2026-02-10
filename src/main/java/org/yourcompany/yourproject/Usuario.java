e2bd85cbbe2bde64d6f02bc8cadf330cee08a2b3
Cristopher Alejandro Santana Lugo <casantanal@centromotosdelhuila.com>
2026-02-10 17:09:49 -0500

    Superclase Usuario creada

---
+++ src/main/java/org/yourcompany/yourproject/Usuario.java
@@ -0,0 +1,21 @@
+package org.yourcompany.yourproject;
+
+public abstract class Usuario {
+    protected String id;
+    protected String nombre;
+    protected String email;
+
+    public Usuario(String id, String nombre, String email) {
+        this.id = id;
+        this.nombre = nombre;
+        this.email = email;
+    }
+
+    public String getNombre() {
+        return this.nombre;
+    }
+
+    public String getEmail() {
+        return this.email;
+    }
+}
