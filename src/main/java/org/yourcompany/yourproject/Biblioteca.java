0232a631b3b639eb050aa20d727a34d829591ebf
Cristopher Alejandro Santana Lugo <casantanal@centromotosdelhuila.com>
2026-02-12 16:56:06 -0500

    Biblioteca

---
+++ src/main/java/org/yourcompany/yourproject/Biblioteca.java
@@ -0,0 +1,26 @@
+package org.yourcompany.yourproject;
+
+import java.util.ArrayList;
+
+public class Biblioteca {
+    private ArrayList<Socio> socios = new ArrayList<>();
+    private ArrayList<Bibliotecario>  bibliotecarios = new ArrayList<>();
+
+    public Biblioteca(){
+
+    }
+    public ArrayList<Socio> getSocios(){
+        return socios;
+    }
+
+    public ArrayList<Bibliotecario> getBibliotecarios() {
+        return bibliotecarios;
+    }
+    public void agregarSocio (Socio socio){
+        socios.add(socio);
+    }
+
+    public void agregarBibliotecario (Bibliotecario bibliotecario){
+        bibliotecarios.add(bibliotecario);
+    }
+}
